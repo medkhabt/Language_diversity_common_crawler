bdf2c7efb50aed43f84d98a8846ae899f9cf346a
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2023-12-14 13:02:30 +0100

    Fix the mapping of languages for the two models

--- commands/count_uniq_lines.sh
+++ commands/count_uniq_lines.sh
@@ -1,19 +1,22 @@
 #!/bin/bash 
 
 # we need a seed first 
- sed 's/.*::\([^:]*\)$/\1/g' $1 > "$1.tmp"
 # input="$1.tmp"; 
 input=$1
 
 declare -A count_words;
 while IFS= read -r line 
 do
-	if [[ -z "${count_words[$line]}" ]]
-	then 
-		let "count_words[$line]=1"
-	else
-		let "count_words[$line]=${count_words[$line]} + 1"
-	fi		
+	if [[ $line != "" ]]
+	    then 
+	    if [[ -z "${count_words[$line]}" ]]
+		then 
+			let "count_words[$line]=1"
+		else
+			let "count_words[$line]=${count_words[$line]} + 1"
+		fi
+	fi
+			
 done < "$input"
 
 for index in "${!count_words[@]}"; do echo "$index    ${count_words[$index]}"; done
--- commands/pre_step_for_count.sh
+++ commands/pre_step_for_count.sh
@@ -1,3 +1,2 @@
 cat $1 |  sed "s/.*::\([^:]*\)$/\1/g" > .pre_count.tmp
-
 bash count_uniq_lines.sh .pre_count.tmp | sort 
