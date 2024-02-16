82e023c9eaff4b371c500f67a6d545c6bf94fb7a
medkhabt <medkha.bt@gmail.com>
2024-02-16 15:39:51 +0100

    Add makefile for execution of one seg

--- v2/commands/fasrawc_generetate_graph_comparaison.sh
+++ v2/commands/fasrawc_generetate_graph_comparaison.sh
@@ -1,29 +1,30 @@
 #! /bin/bash 
 
 ## Args 1 : dat file name 
-dat_file_with_path=../graphs/dat/$1.dat
+dat_file_with_path=graphs/dat/$1.dat
 ## Args 2 : dem file name 
-dem_file_with_path=../graphs/dem/$2.dem
+dem_file_with_path=graphs/dem/$2.dem
+
 ## access the dem folder .
 ## cd ../graphs/dem 2> /dev/null
 ## Gen dir and  Generate the file splits for the specified file in args. 
-rm -r ../graphs/dat/.$1 2>/dev/null
-mkdir ../graphs/dat/.$1 2> /dev/null
+rm -r graphs/dat/.$1 2>/dev/null
+mkdir graphs/dat/.$1 2> /dev/null
 ## Run the split script 
 lines=$(wc -l $dat_file_with_path | awk '{print $1}')
 file_name=$(echo $1 | awk -F . '{print $1}' )
 let "splits = (lines/10) + 1"
 for ((i=0 ; i < $splits; i++)); do 
     let "x=i*10+2"
-    head -n 1 $dat_file_with_path > ../graphs/dat/.$1/${1}_$i.dat
-    tail -n +$x $dat_file_with_path | head -n 10 >> ../graphs/dat/.$1/${1}_$i.dat
+    head -n 1 $dat_file_with_path > graphs/dat/.$1/${1}_$i.dat
+    tail -n +$x $dat_file_with_path | head -n 10 >> graphs/dat/.$1/${1}_$i.dat
 done
 
 ## Get all the files in a list 
 # TODO check if this works.
-split_files=$(ls ../graphs/dat/.$1 -pl | grep -v / | awk '{if(NR-1>0) print $NF}')
+split_files=$(ls graphs/dat/.$1 -pl | grep -v / | awk '{if(NR-1>0) print $NF}')
 
-cd ../graphs/dem
+cd graphs/dem
 
 for file in $split_files 
 do 
